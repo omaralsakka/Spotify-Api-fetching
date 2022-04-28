6db5b91e4b4c5996790663b0492ab03dd9441453
Erik Mende <emende@student.hive.fi>
2022-04-28 20:20:24 +0300

    No PHP used here, changed to .html files

--- login.js
+++ login.js
@@ -1,6 +1,6 @@
 var	client_id = '8edabb1f4f7e4c60bfb0730950ddc7d7',
 	client_secret = 'ad9281dc77754a50a4c9cc6c7e93e854',
-	redirect_uri = encodeURI('http://localhost:8080/Spotify/callback.php'),
+	redirect_uri = encodeURI('http://localhost:8080/Spotify/callback.html'),
 	scopes = "user-read-private user-read-email user-modify-playback-state " +
 	"user-read-playback-position user-library-read streaming " +
 	"user-read-playback-state user-read-recently-played playlist-read-private";
